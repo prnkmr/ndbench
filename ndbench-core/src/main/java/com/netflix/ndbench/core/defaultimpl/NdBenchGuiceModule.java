d38b39ccb75b00791cf780fbbe8bea6d1d836a9a
praveenkumar-E1444 <praveenkumar.ravikumar@freshworks.com>
2018-12-18 15:10:14 +0530

    config changes.

--- ndbench-core/src/main/java/com/netflix/ndbench/core/defaultimpl/NdBenchGuiceModule.java
+++ ndbench-core/src/main/java/com/netflix/ndbench/core/defaultimpl/NdBenchGuiceModule.java
@@ -28,6 +28,7 @@ import com.netflix.ndbench.core.config.IConfiguration;
 import com.netflix.ndbench.core.config.NdbenchConfigListener;
 import com.netflix.ndbench.core.discovery.AWSLocalClusterDiscovery;
 import com.netflix.ndbench.core.discovery.CfClusterDiscovery;
+import com.netflix.ndbench.core.discovery.ConfigFileDiscovery;
 import com.netflix.ndbench.core.discovery.IClusterDiscovery;
 import com.netflix.ndbench.core.discovery.LocalClusterDiscovery;
 import com.netflix.ndbench.core.generators.DefaultDataGenerator;
@@ -49,6 +50,8 @@ public class NdBenchGuiceModule extends AbstractModule {
             bind(IClusterDiscovery.class).to(CfClusterDiscovery.class);
         } else if (discoveryEnv != null && discoveryEnv.equals(NdBenchConstants.DISCOVERY_ENV_AWS)) {
             bind(IClusterDiscovery.class).to(AWSLocalClusterDiscovery.class);
+        } else if(discoveryEnv != null && discoveryEnv.equals(NdBenchConstants.DISCOVERY_ENV_CONFIG)){
+            bind(IClusterDiscovery.class).to(ConfigFileDiscovery.class);
         } else {
             bind(IClusterDiscovery.class).to(LocalClusterDiscovery.class);
         }
